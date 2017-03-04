3160e61a98ee228cf901e142187cc791d9dc76ce
David Piper <dbpiper@gmail.com>
2017-03-04 08:26:36 -0600

    Fix bubble sort, problem was caused by the swap function not using the correct variables.

--- sorting/basicSorts.c
+++ sorting/basicSorts.c
@@ -1,13 +1,30 @@
 #include "basicSorts.h"
 
+void swap(int *array, int i, int j)
+{
+    int temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}
+
 void insertionSort(int* array, int len)
 {
     int i, j;
     for (i=1; i<len; i++) {
         for (j=i; array[j-1] > array[j]; j--) {
-            int temp = array[j-1];
-            array[j-1] = array[j];
-            array[j] = temp;
+            swap(array, j, j-1);
+        }
+    }
+}
+
+void bubbleSort(int* array, int len)
+{
+    int i, j;
+    for (i=0; i<len; i++) {
+        for (j=len-1; j>=i+1; j--) {
+            if (array[j] < array[j-1]) {
+                swap(array, j, j-1);
+            }
         }
     }
 }
